4cfc911cff46d58453d5f1270f8bf72f0f3138d0
kento-oka <kento-oka@kentoka.com>
2018-11-01 16:41:20 +0900

    定義されているルートリストを表示するコマンドを作成

---
+++ src/Command/RouteShowCommand.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * FratilyPHP Framework Bundle
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author      Kento Oka <kento-oka@kentoka.com>
+ * @copyright   (c) Kento Oka
+ * @license     MIT
+ * @since       1.0.0
+ */
+namespace Fratily\Bundle\Framework\Controller;
+
+use Fratily\Router\RouteCollector;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\Table;
+
+/**
+ *
+ */
+class RouteShowCommand extends Command{
+
+    const HELP  = <<<HELP
+This is routing command.
+HELP;
+
+    /**
+     * @var RouteCollector
+     */
+    private $routeCollector;
+
+    public function __construct(RouteCollector $routeCollector){
+        parent::__construct();
+
+        $this->routeCollector   = $routeCollector;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(){
+        $this
+            ->setName("debug:router:show")
+            ->setDefinition([])
+            ->setDescription("Displays current routes for an application")
+            ->setHelp(self::HELP)
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output){
+        $table  = new Table($output);
+
+
+        $table->setHeaders(["Name", "Path", "Method", "Allows"]);
+
+        foreach($this->routeCollector->getAll() as $route){
+            $table->addRow([
+                $route->getName(),
+                $route->getPath(),
+                $route->getHost(),
+                implode(", ", $route->getAllows())
+            ]);
+        }
+
+        $table->render();
+    }
+}
